c1fc416a0bda58071447c59b84f54a1a954ce89f
Kumongoodlife <kumongoodlife@gmail.com>
2017-11-04 12:45:45 +0900

    テストケース書き始めた。

--- tests/Errorcheck/ChangenameErrorTest.php
+++ tests/Errorcheck/ChangenameErrorTest.php
@@ -3,14 +3,21 @@ namespace Kumon\KeizibanLib\Errorcheck;
 
 use PHPUnit\Framework\TestCase;
 
+use Kumon\KeizibanLib\System\Encode;
+use Kumon\KeizibanLib\System\DbManager;
+
+use Kumon\KeizibanLib\Errorcheck\ChangenameError;
 use Kumon\KeizibanLib\Errorcheck\PassException;
 use Kumon\KeizibanLib\Errorcheck\ChangenameException;
 
-class ChangenameErrorTest {
+class ChangenameErrorTest extends Testcase {
 
   public function setUp() {
-    
+    $db = DbManager::getDbforTest();
   }
+
+  
+
 
 
 }
